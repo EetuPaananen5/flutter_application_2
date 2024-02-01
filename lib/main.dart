f856c2d4c4eb991266d9e3ec4447a786917e6a62
EetuPaananen5 <117155998+EetuPaananen5@users.noreply.github.com>
2024-02-01 12:51:55 +0200

    Login firebaseen toimii Pixel 7 pro api 30 puhelimella

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_application_2/views/input_view.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,11 +26,41 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+      final providers = [EmailAuthProvider()];
     return ChangeNotifierProvider(
       create: (context) => TaskData(),
-      child: const MaterialApp(
+      child:  MaterialApp(
         title: 'Task Manager',
-        home: InputView(),
+
+initialRoute:
+            FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/main-view',
+        routes: {
+          '/sign-in': (context) {
+            return SignInScreen(
+              providers: providers,
+              actions: [
+                AuthStateChangeAction<SignedIn>((context, state) {
+                  Navigator.pushReplacementNamed(context, '/main-view');
+                }),
+              ],
+            );
+          },
+          '/profile': (context) {
+            return ProfileScreen(
+              providers: providers,
+              actions: [
+                SignedOutAction((context) {
+                  Navigator.pushReplacementNamed(context, '/sign-in');
+                }),
+              ],
+            );
+          },
+          '/main-view': (context) {
+            return const InputView();
+          },
+        },
+
+        //home: InputView(),
       ),
     );
   }
